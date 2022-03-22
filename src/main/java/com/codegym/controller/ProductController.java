82bec22498b13cea875eb1c52fdfe0f4c535a369
kieuhuan <kieuhuanbka@gmail.com>
2022-03-22 14:10:10 +0700

    first commit

---
+++ src/main/java/com/codegym/controller/ProductController.java
@@ -0,0 +1,91 @@
+package com.codegym.controller;
+
+import com.codegym.model.Product;
+import com.codegym.model.ProductForm;
+import com.codegym.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+public class ProductController {
+    @Autowired
+    private IProductService productService;
+
+    @Value("${file-upload}")
+    private String uploadPath;
+
+    @GetMapping("/products/list")
+    public ModelAndView showListProduct() {
+        ModelAndView modelAndView = new ModelAndView("/product/list");
+        List<Product> products = productService.findAll();
+        modelAndView.addObject("products", products);
+        return modelAndView;
+    }
+
+    @GetMapping("/products/delete/{id}")
+    public ModelAndView showDeleteProduct(@PathVariable Integer id) {
+        ModelAndView modelAndView = new ModelAndView("/product/delete");
+        Product product = productService.findById(id);
+        modelAndView.addObject("product", product);
+        return modelAndView;
+    }
+
+    @PostMapping("/products/delete/{id}")
+    public ModelAndView deleteProduct(@PathVariable Integer id) {
+        productService.removeById(id);
+        return new ModelAndView("redirect:/products/list");
+    }
+
+    @GetMapping("/products/create")
+    public ModelAndView showCreateProduct() {
+        ModelAndView modelAndView = new ModelAndView("/product/create");
+        modelAndView.addObject("product", new ProductForm());//Gửi 1 đối tượng product rỗng sang file view để tạo mới
+        return modelAndView;
+    }
+
+    @PostMapping("/products/create")
+    public ModelAndView createProduct(@ModelAttribute ProductForm productForm) {
+        String fileName = productForm.getImage().getOriginalFilename();
+        long currentTime = System.currentTimeMillis(); //Xử lý lấy thời gian hiện tại
+        fileName = currentTime + fileName;
+        try {
+            FileCopyUtils.copy(productForm.getImage().getBytes(), new File(uploadPath + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Product product = new Product(productForm.getId(), productForm.getName(), productForm.getPrice(), productForm.getDescription(), fileName);
+        productService.create(product);
+        return new ModelAndView("redirect:/products/list");
+    }
+
+    @GetMapping("/products/edit/{id}")
+    public ModelAndView showEditForm(@PathVariable Integer id) {
+        ModelAndView modelAndView = new ModelAndView("/product/edit");
+        Product product = productService.findById(id);
+        modelAndView.addObject("product", product);
+        return modelAndView;
+    }
+
+    @PostMapping("/products/edit/{id}")
+    public ModelAndView editProduct(@PathVariable Integer id, @ModelAttribute Product product) {
+        productService.updateById(id, product);
+        return new ModelAndView("redirect:/products/list");
+    }
+
+    @GetMapping("/products/{id}")
+    public ModelAndView showProductDetail(@PathVariable Integer id) {
+        Product product = productService.findById(id);
+        return new ModelAndView("/product/view", "product", product);
+    }
+}
